96afbb8f4ba487ec41ebf514c3f1f6ce9f334aca
Paulo Cezar de Souza Filho <Paulo.2012@hotmail.com>
2021-08-30 23:59:38 -0300

    refatora codigo

--- MemeMaker.js
+++ MemeMaker.js
@@ -7,15 +7,25 @@ const streamOptions = { seek: 0, volume: 1 };
 bot.login(process.env.TOKEN);
 
 bot.on("ready", () => {
-  //message.channel.send("Digita um !Info pra ver os comandos disponiveis e umas fita a mais ae")
   console.log('estou pronto para ser usado"!');
 });
+
+const commands = {
+  rapaz: "https://www.youtube.com/watch?v=Jvl0L9GRH6o",
+  uepa: "https://youtu.be/UpNy0v_ur-o",
+  pare: "https://www.youtube.com/watch?v=QO7fw-OcOD4",
+  irra: "https://youtu.be/CSDVrF5gAus",
+};
 
 bot.on("message", (msg) => {
   if (msg.author.bot) {
     return;
   }
-
+  const [prefixo, ...rest] = msg.content;
+  const command = rest.join("");
+  if (prefixo !== "!") {
+    return;
+  }
   function toca(youtubeLink) {
     let VoiceChannel = msg.guild.channels.cache.find(
       (channel) => channel.id === "832066730477879322"
@@ -41,31 +51,11 @@ bot.on("message", (msg) => {
     }
   }
 
-  if (msg.content === "!info") {
+  if (command === "info") {
     msg.channel.send(
       "Projeto F.F.F (Futebol, Feijoada & Funk) é um bot que reproduz efeitos sonoros classicos da tv Brasileira desde o 'Rapaz' do xaropinho, até o João gordo brigando com o dolabella.ㅤㅤㅤㅤㅤㅤㅤㅤ ------------Comandos Disponiveis (No Momento): '!Rapaz', '!Irra', '!Uepa' e '!Pare------------"
     );
   }
-  if (msg.content === "!rapaz") {
-    toca("https://www.youtube.com/watch?v=Jvl0L9GRH6o");
-  }
-  if (msg.content === "!uepa") {
-    toca("https://youtu.be/UpNy0v_ur-o");
-  }
-  if (msg.content === "!pare") {
-    toca("https://www.youtube.com/watch?v=QO7fw-OcOD4");
-  }
-  if (msg.content === "!irra") {
-    toca("https://youtu.be/CSDVrF5gAus");
-  }
-});
 
-/*const executor = muse => {
-    const muses = {
-      Rapaz :'https://www.youtube.com/watch?v=Jvl0L9GRH6o',
-      Uepa :'https://youtu.be/UpNy0v_ur-o',
-      Pare :'https://www.youtube.com/watch?v=QO7fw-OcOD4',
-      Irra :'https://youtu.be/CSDVrF5gAus'
-    }
-    return muses[muse]
-}*/
+  toca(commands[command]);
+});
