98ac9ee2a6358ddc2382fa4f7a1d9d4efba0cdaf
Paulo Cezar de Souza Filho <Paulo.2012@hotmail.com>
2021-09-02 21:08:30 -0300

    pronto pra uso

--- MemeMaker.js
+++ MemeMaker.js
@@ -15,6 +15,18 @@ const commands = {
   uepa: "https://youtu.be/UpNy0v_ur-o",
   pare: "https://www.youtube.com/watch?v=QO7fw-OcOD4",
   irra: "https://youtu.be/CSDVrF5gAus",
+  qisso:
+    "https://www.youtube.com/watch?v=ou3BlCaj1DM&list=PLJxFlYsd38rLghWxPrtoX2mL7jXCrZq_M&index=9",
+  ratinho:
+    "https://www.youtube.com/watch?v=-d6wB4KyuUk&list=PLJxFlYsd38rLghWxPrtoX2mL7jXCrZq_M&index=11",
+  tarzan:
+    "https://www.youtube.com/watch?v=dvZIySi9-5Q&list=PLJxFlYsd38rLghWxPrtoX2mL7jXCrZq_M&index=15",
+  grito:
+    "https://www.youtube.com/watch?v=RM88KhLw0oA&list=PLRyDuP997asbuX7phvrSD8-LKhcvEXHox&index=16",
+  gatinho:
+    "https://www.youtube.com/watch?v=OutIegT8HBg&list=PLRyDuP997asbuX7phvrSD8-LKhcvEXHox&index=10",
+  suspense:
+    "https://www.youtube.com/watch?v=GS7tHGWMDS4&list=PLier33pDdoyejhHwEqeKSST93RV_BKIig&index=19",
 };
 
 bot.on("message", (msg) => {
@@ -51,11 +63,21 @@ bot.on("message", (msg) => {
     }
   }
 
-  if (command === "info") {
+  if (command === "iinfo") {
     msg.channel.send(
-      "Projeto F.F.F (Futebol, Feijoada & Funk) é um bot que reproduz efeitos sonoros classicos da tv Brasileira desde o 'Rapaz' do xaropinho, até o João gordo brigando com o dolabella.ㅤㅤㅤㅤㅤㅤㅤㅤ ------------Comandos Disponiveis (No Momento): '!Rapaz', '!Irra', '!Uepa' e '!Pare------------"
+      "Projeto F.F.F (Futebol, Feijoada & Funk) é um bot que reproduz efeitos sonoros classicos da tv Brasileira desde o 'Rapaz' do xaropinho, até o João gordo brigando com o dolabella.ㅤㅤㅤㅤㅤㅤㅤㅤ ------------Comandos Disponiveis (No Momento): " +
+        commands +
+        "------------"
     );
+    return;
   }
 
-  toca(commands[command]);
+  const chosen = commands[command];
+  if (chosen) {
+    toca(chosen);
+    return;
+  }
+  msg.channel.send(
+    "Este comando não existe, digite !iinfo para ver os comandos existentes."
+  );
 });
